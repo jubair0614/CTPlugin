f66f5fcac673e8767d06ae91b2366f56586720f6
Md. Jubair Ibna Mostafa <bsse0614@iit.du.ac.bd>
2017-11-14 18:21:05 +0600

    nicad4 runner script updated

--- resources/scripts/runNicad4.sh
+++ resources/scripts/runNicad4.sh
@@ -3,18 +3,25 @@
 echo $line
 echo "Input File Building..........."
 
-echo $1
     cd /home/jubair/SPL3/tools/NiCad-4.0
 
     nicad4 functions java $1
 
+    echo "Project full path is: ${1}"
+
+    IFS=/
+    ary=($1)
+    projectName=${ary[-1]}
+
+    echo "Project is $projectName"
+
     echo "File copying in the output directory"
-	cp -r /home/jubair/SPL3/test_projects/JHotDraw54b1_functions-clones /home/jubair/SPL3/test_projects/cloneResult/
-	cp /home/jubair/SPL3/test_projects/JHotDraw54b1_functions.xml /home/jubair/SPL3/test_projects/cloneResult/JHotDraw54b1_functions-clones/JHotDraw54b1_functions.xml
+	cp -r /home/jubair/SPL3/test_projects/"${projectName}_functions-clones" /home/jubair/SPL3/test_projects/cloneResult/
+	cp /home/jubair/SPL3/test_projects/"${projectName}_functions.xml" /home/jubair/SPL3/test_projects/cloneResult/"${projectName}_functions-clones"/"${projectName}_functions.xml"
 
 	echo "Cleaning  directory"
-	rm -r /home/jubair/SPL3/test_projects/JHotDraw54b1_functions-clones
-	rm -f /home/jubair/SPL3/test_projects/JHotDraw54b1_functions.xml
-	rm -f /home/jubair/SPL3/test_projects/JHotDraw54b1_functions-clones-*
+	rm -r /home/jubair/SPL3/test_projects/"${projectName}_functions-clones"
+	rm -f /home/jubair/SPL3/test_projects/"${projectName}_functions.xml"
+	rm -f /home/jubair/SPL3/test_projects/"${projectName}_functions-clones-"*
 
 echo "Successfully Done"
