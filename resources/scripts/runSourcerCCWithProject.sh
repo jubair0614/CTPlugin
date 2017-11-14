9b32c541d17b2b334f6892cea19d383f18ba73bb
Md. Jubair Ibna Mostafa <bsse0614@iit.du.ac.bd>
2017-11-14 21:09:52 +0600

    script updated

--- resources/scripts/runSourcerCCWithProject.sh
+++ resources/scripts/runSourcerCCWithProject.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-mkdir /home/jubair/SPL3/test_projects/cloneResult/dnsjava
-
 echo $line
 echo $1
 
+    IFS=/
+    ary=($1)
+    projectName=${ary[-1]}
+
+    echo "Project is $projectName"
+    mkdir /home/jubair/SPL3/test_projects/cloneResult/${projectName}
+
     echo "Input File Building..........."
 	cd /home/jubair/SPL3/tools/SourcererCC/parser/java
-	java -jar InputBuilderClassic.jar $1/$line /home/jubair/SPL3/tools/SourcererCC/input/dataset/tokens.file /home/jubair/SPL3/tools/SourcererCC/input/bookkeping/headers.file functions java 0 0 0 0 false false false 8
+	java -jar InputBuilderClassic.jar $1 /home/jubair/SPL3/tools/SourcererCC/input/dataset/tokens.file /home/jubair/SPL3/tools/SourcererCC/input/bookkeping/headers.file functions java 0 0 4 0 false false false 8
 
 	echo "Indexing input.............."
 	cd /home/jubair/SPL3/tools/SourcererCC/
@@ -17,9 +22,9 @@ echo $1
 	java -jar dist/indexbased.SearchManager.jar search 8
 
 	echo "File copying in the output directory"
-	mkdir /home/jubair/SPL3/test_projects/cloneResult/dnsjava/$line
-	cp /home/jubair/SPL3/tools/SourcererCC/input/bookkeping/headers.file /home/jubair/SPL3/test_projects/cloneResult/dnsjava/$line
-	cp /home/jubair/SPL3/tools/SourcererCC/output8.0/tokensclones_index_WITH_FILTER.txt /home/jubair/SPL3/test_projects/cloneResult/dnsjava/$line
+	mkdir /home/jubair/SPL3/test_projects/cloneResult/${projectName}
+	cp /home/jubair/SPL3/tools/SourcererCC/input/bookkeping/headers.file /home/jubair/SPL3/test_projects/cloneResult/${projectName}/headers.file
+	cp /home/jubair/SPL3/tools/SourcererCC/output8.0/tokensclones_index_WITH_FILTER.txt /home/jubair/SPL3/test_projects/cloneResult/${projectName}/tokensclones_index_WITH_FILTER.txt
 
 	echo "Cleaning  directory"
 	rm -r fwdindex gtpm index output output8.0
@@ -27,7 +32,7 @@ echo $1
 	rm -f /home/jubair/SourcererCC/input/bookkeping/headers.file
 
 	echo "Renamining Output Directory file"
-	cd /home/jubair/SPL3/test_projects/cloneResult/dnsjava/$line
+	cd /home/jubair/SPL3/test_projects/cloneResult/${projectName}
 	mv headers.file PathID.txt
 	mv tokensclones_index_WITH_FILTER.txt ClonePairID_Similarity_80.txt
 
