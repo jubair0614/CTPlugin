e2d0f038fbb1babaf90b11cdd781a30e374bf219
Md. Jubair Ibna Mostafa <bsse0614@iit.du.ac.bd>
2017-11-26 13:27:04 +0600

    unused code is deleted

--- src/tracker/Manager.java
+++
@@ -1,8 +0,0 @@
-package tracker;
-
-/**
- * Created by jubair on 11/23/17.
- * Time 1:22 AM
- */
-public class Manager {
-}
--- src/tracker/SearchClone.java
+++
@@ -1,9 +0,0 @@
-package tracker;
-
-/**
- * Created by jubair on 11/23/17.
- * Time 3:51 AM
- */
-public class SearchClone {
-
-}
